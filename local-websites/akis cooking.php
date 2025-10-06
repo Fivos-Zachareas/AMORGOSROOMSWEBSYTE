c6c1c20cb5427ed08dd0c5aa077f0a3622d3936d
Fivos <164408883+Fivos-Zachareas@users.noreply.github.com>
2025-10-06 13:04:10 +0300

    Add files via upload

---
+++ local-websites/akis cooking.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="el">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="CSS/style-form.css">
+</head>
+<body>
+    
+    <div class="layout">
+        <div class="form-container" style = "border: 5px solid DodgerBlue">
+            <h1>akis cooking</h1>
+            
+            </form>
+        </div>
+    </div>
+    <div class = "form-container" style = "border: 0px">
+        <img src="images/find-a-room/akis-cooking.jpeg"  style="float:left;width:20%;height:20%;">
+        <p>
+        11
+        </p>
+    </div>
+    <div class = "form-container" style = "border: 5px solid DodgerBlue">
+        Κινητό: 1 &emsp;
+        Σταθερό: 111111 &emsp;
+        Email: 1@1
+    </div>
+    
+</body>
+</html>
